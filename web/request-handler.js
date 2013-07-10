7f2c8274a20b7aedf5ee5c51c4b9d30cfe78f0bc
Cameron Boehmer <cameron.boehmer@gmail.com>
2013-07-10 15:22:47 -0700

    path fix in request-handler

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,5 +1,6 @@
-exports.datadir = __dirname + "data/sites.txt"; // tests will need to override this.
+var path = require('path');
+module.exports.datadir = path.join(__dirname, "../data/sites.txt"); // tests will need to override this.
 
-exports.handleRequest = function (req, res) {
+module.exports.handleRequest = function (req, res) {
   console.log(exports.datadir);
 };
